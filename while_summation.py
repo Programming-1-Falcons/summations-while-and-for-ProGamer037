0a5563f1d8fa6bbf168c179b9cfbc37dd20540a2
Beckett Christoffersen <105308722+ProGamer037@users.noreply.github.com>
2024-11-07 09:42:35 -0700

    Add files via upload

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
-#while summation code following directions from Readme
+number = int(input("Enter a number: "))
+sum_while = 0
+current = 1
+
+while current < number:
+    sum_while += current
+    current += 1
+    
+print("Sum: ", sum_while + number)
